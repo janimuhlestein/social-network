1fc49ca36839706f37a7597905baf4aa1aff2282
Jani Muhlestein <janimuhlestein@gmail.com>
2020-12-04 16:14:39 -0700

    adding users and routes

---
+++ controllers/user-controller.js
@@ -0,0 +1,82 @@
+const {User} = require('../models');
+
+const userController = {
+    //find all users
+    getAllUsers(req,res) {
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort('_id: -1')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+    //get one use by id
+    getUserById({params}, res) {
+        User.findOne({_id: params.id})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData=>{
+            if(!dbUserData) {
+                res.status(404).json({message: 'No user found with this id'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+    //create user with POST
+    createUser({body}, res) {
+        User.create(body)
+        .then(dbUserData=>{
+            res.json(dbUserData)
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+    //update user by id
+    updateUser({params}, res) {
+        User.findOneAndUpdate({_id: params.id}, body, {new: true, runValiudators: true})
+            .then(dbUserData=>{
+                if(!dbUserData) {
+                    res.status(404).json({message: 'No user found with this id'});
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err=>{
+                console.log(err);
+                res.status(500).json(err);
+            });
+        },
+    //delete user by id
+    deleteUser({params}, res) {
+        User.findOneAndDelete({_id: params.id})
+        .then(dbUserId=>{
+            if(!dbUserData){
+                res.status(404).json({message: 'No user found with that id'});
+                return;
+            }
+            res.json(dbUserId);
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json(err);
+        });
+    }
+};
+
+module.exports = userController;
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 app.use(require('./routes'));
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
@@ -12,6 +15,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network'
     useUnifiedTopology: true
 });
 
-mongoose.set('debut', true);
+mongoose.set('debug', true);
 
 app.listen(PORT, ()=> console.log(`Connected on localhost: ${PORT}`));
